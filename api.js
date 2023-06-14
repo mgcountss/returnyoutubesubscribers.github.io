9c21ca10a84a9d4afc6f4ed1bc8f867ba4f91dcd
MGCounts <68518563+mgcountss@users.noreply.github.com>
2023-06-13 22:22:09 -0500

    Update api.js

--- api.js
+++ api.js
@@ -12,46 +12,30 @@ app.get('/', function (req, res) {
 app.get('/:id', (req, res) => {
     if (req.params.id.length == 24) {
         if (req.params.id.startsWith('UC')) {
-            axios.get(process.env.ncapi + "" + req.params.id)
-                .then(response => {
-                    if (response.data.success == false) {
-                        next()
-                        console.log('next nc to xyz')
-                    } else {
-                        res.status(200).send({ "success": true, "count": response.data.subs, "verified": true });
-                    }
-                }).catch(err => {
-                    next()
-                    console.log('next nc to xyz')
-                })
+            axios.get(process.env.lcapi + "" + req.params.id)
+            .then(response => {
+                if (response.data.isStudio == true) {
+                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": true, "by": "✓ by Axern.space" });
+                } else {
+                    axios.get(process.env.ncapi + "" + req.params.id)
+                        .then(response2 => {
+                            if (response2.data.success == false) {
+                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "✓ by Axern.space" });
+                            } else {
+                                res.status(200).send({ "success": true, "count": response2.data.subs, "verified": true, "by": "✓ by NextCounts" });
+                            }
+                        }).catch(err => {
+                            res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
+                        })
+                }
+            }).catch(err => {
+                res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
+            })
         } else {
             res.send({ "success": false, "count": null, "verified": false, "msg": "invalid channel id" })
         }
     } else {
         res.send({ "success": false, "count": null, "verified": false, "msg": "invalid channel id" })
-    }
-    function next() {
-        axios.get(process.env.lcapi + "" + req.params.id)
-            .then(response => {
-                if (response.data.counts[0] == null) {
-                    console.log('next xyz to mx')
-                    next2()
-                } else {
-                    res.status(200).send({ "success": true, "count": response.data.counts[0], "verified": false });
-                }
-            }).catch(err => {
-                console.log('next xyz to mx')
-                next2()
-            })
-    }
-    function next2() {
-        axios.get(process.env.mxapi + "" + req.params.id)
-            .then(response => {
-                res.status(200).send({ "success": true, "count": response.data.subscribers, "verified": false });
-            }).catch(err => {
-              console.log('failed all', req.params.id)
-                res.status(200).send({ "success": false, "count": null, "verified": false, "msg": "unknown channel" })
-            })
     }
 })
 
