9578d37bc00a37449ac20b71bb2fe3c68320ad5d
MGCounts <68518563+mgcountss@users.noreply.github.com>
2024-01-13 17:01:30 -0600

    now uses yabcounts

--- api.js
+++ api.js
@@ -1,36 +1,48 @@
-var express = require('express')
-var app = express()
-const cors = require('cors')
-const axios = require('axios');
+
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
 app.use(cors())
 
 app.get('/', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.send("ok")
+    res.redirect('https://ryts.mgcounts.com')
 })
 
 app.get('/:id', (req, res) => {
     if (req.params.id.length == 24) {
         if (req.params.id.startsWith('UC')) {
-            axios.get(process.env.lcapi + "" + req.params.id)
-            .then(response => {
-                if (response.data.isStudio == true) {
-                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": true, "by": "✓ by Axern.space" });
-                } else {
-                    axios.get(process.env.ncapi + "" + req.params.id)
-                        .then(response2 => {
-                            if (response2.data.success == false) {
-                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "✓ by Axern.space" });
-                            } else {
-                                res.status(200).send({ "success": true, "count": response2.data.subs, "verified": true, "by": "✓ by NextCounts" });
-                            }
-                        }).catch(err => {
-                            res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
-                        })
-                }
-            }).catch(err => {
+            try {
+            axios.get(process.env.axapi + "" + req.params.id)
+                .then(response => {
+                    if (response.data.isStudio == true) {
+                        res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": true, "by": "✓" });
+                    } else {
+                        axios.get(process.env.yabap.replace('{channel}', req.params.id))
+                            .then(response2 => {
+                                if ((response2) && (response2.data.statistics) && (response2.data.statistics.subscriberCount)) {
+                                    res.status(200).send({ "success": true, "count": response.data.statistics.subscriberCount, "verified": false, "by": "| yabcounts.com" });
+                                } else {
+                                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "axern.space" });
+                                }
+                            }).catch(err => {
+                                if (response.data.estSubCount) {
+                                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "axern.space" });
+                                } else {
+                                    res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
+                                }
+                            })
+                    }
+                }).catch(err => {
+                    console.log(err)
+                    res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
+                })
+            } catch (err) {
                 res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
-            })
+            }
         } else {
             res.send({ "success": false, "count": null, "verified": false, "msg": "invalid channel id" })
         }
@@ -39,4 +51,4 @@ app.get('/:id', (req, res) => {
     }
 })
 
-app.listen(process.env.port || 80)
+app.listen(1111)
