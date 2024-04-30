45aa3cc208fcb4af5baf8e8f7a00ab3fdd94ae84
MGCounts <68518563+mgcountss@users.noreply.github.com>
2024-04-29 23:35:28 -0500

    Update api.js

--- api.js
+++ api.js
@@ -15,30 +15,18 @@ app.get('/:id', (req, res) => {
     if (req.params.id.length == 24) {
         if (req.params.id.startsWith('UC')) {
             try {
-            axios.get(process.env.axapi + "" + req.params.id)
-                .then(response => {
-                    if (response.data.isStudio == true) {
-                        res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": true, "by": "✓" });
-                    } else {
-                        axios.get(process.env.yabapi.replace('{channel}', req.params.id))
-                            .then(response2 => {
-                                if ((response2) && (response2.data.statistics) && (response2.data.statistics.subscriberCount)) {
-                                    res.status(200).send({ "success": true, "count": response2.data.statistics.subscriberCount, "verified": false, "by": "| yabcounts.com" });
-                                } else {
-                                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "axern.space" });
-                                }
-                            }).catch(err => {
-                                console.log(err)
-                                if (response.data.estSubCount) {
-                                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": false, "by": "axern.space" });
-                                } else {
-                                    res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
-                                }
-                            })
-                    }
+                axios.get(process.env.niaapi + "" + req.params.id, {
+                    timeout: 1750
+                }).then(response => {
+                    res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": response.data.isStudio, "by": "nia-statistics.com" });
                 }).catch(err => {
-                    console.log(err)
-                    res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
+                    axios.get(process.env.axapi + "" + req.params.id, {
+                        timeout: 1750
+                    }).then(response => {
+                        res.status(200).send({ "success": true, "count": response.data.estSubCount, "verified": response.data.verified, "by": "axern.space" });
+                    }).catch(err => {
+                        res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" });
+                    })
                 })
             } catch (err) {
                 res.status(200).send({ "success": false, "count": 0, "verified": false, "msg": "unknown channel" })
@@ -51,4 +39,4 @@ app.get('/:id', (req, res) => {
     }
 })
 
-app.listen(1111)
+app.listen(1111);
